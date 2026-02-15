790cf3d005575fa6c8f7fb42ede6c9dd96c08066
Aravind <mannearavind07@gmail.com>
2026-02-15 12:59:39 +0530

    BE

--- src/app.js
+++ src/app.js
@@ -12,4 +12,28 @@ const notes = []
 
 
 
+app.post('/notes', async (req, res) => {
+
+    const data = req.body;
+    await noteModel.create({
+        title: data.title,
+        description: data.description
+
+    })
+    res.status(201).json({
+        message: "Note created successfully"
+    })
+})
+
+app.get('/notes', async (req, res) => {
+
+    const notes = await noteModel.find()  // give data in array format 
+
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes: notes
+    })
+
+})
+
 module.exports = app
