65c6cd3dc3d4044bb2eb6d4315d0ae61d435156c
Aravind <mannearavind07@gmail.com>
2026-02-15 13:28:47 +0530

    BE

--- src/app.js
+++ src/app.js
@@ -36,4 +36,36 @@ app.get('/notes', async (req, res) => {
 
 })
 
+
+app.delete('/notes/:id', async (req, res) => {
+
+    const id = req.params.id;
+
+    await noteModel.findByIdAndDelete({
+        _id: id
+    })
+    res.status(200).json({
+        message: "Note deleted successfully"
+    })
+
+})
+
+
+app.patch('/notes/:id', async (req, res) => {
+
+    const id = req.params.id;
+    const describtion = req.body.describtion;
+
+    await noteModel.findOneAndUpdate({
+        _id: id
+    }, {
+        description: describtion
+    })
+     res.status(200).json({
+        message: "Note updated successfully"
+    })
+
+})
+
+
 module.exports = app
