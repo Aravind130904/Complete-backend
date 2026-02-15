ccaa95f2d53f5d768eb649936710b095a0a9ab81
Aravind <mannearavind07@gmail.com>
2026-02-15 13:00:01 +0530

    BE

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
 
-    await mongoose.connect("your mongodb cluster connection")
+    await mongoose.connect("mongodb+srv://backend:1309@backend.cqbntnj.mongodb.net/hello")
 
     console.log("DB connected successfully")
 
